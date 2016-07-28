9857891d875773c94fc8fa88237ff907a63b3776
memoizr <memoizrlabs@gmail.com>
2016-07-28 12:43:55 +0100

    add expect

--- src/main/kotlin/com/memozr/assertk/General.kt
+++ src/main/kotlin/com/memozr/assertk/General.kt
@@ -1,6 +1,7 @@
 package com.memozr.assertk
 
 val assert: AssertionHook get() = AssertionHook()
+val expect: AssertionHook get() = AssertionHook()
 
 class AssertionHook {
     infix fun <T : Any> that(subjectUnderTest: T?) = AbstractAssertBuilder(subjectUnderTest)
